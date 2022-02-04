58eaffc55c8e68df717fa23fea0680f9a057c416
AntiEevee <eefjekarremans@hotmail.nl>
2022-02-04 01:47:08 +0100

    Commend Admin addeds

--- src/main/java/nl/hsleiden/svdj8/Svdj8Application.java
+++ src/main/java/nl/hsleiden/svdj8/Svdj8Application.java
@@ -36,10 +36,10 @@ public class Svdj8Application {
         return args -> {
 //				adminDAO.addAdmin(new Admin(null,"Eevee","root112","Admin"));
 //							adminDAO.addAdmin(new Admin(null,"Eevee2","3ff1d66d2b6f0a0121f7a88d4de4d75d","Admin"));
-            adminDAO.addAdmin(new Admin(null, "Admin", "114663ab194edcb3f61d409883ce4ae6c3c2f9854194095a5385011d15becbef" //admin12
-                    , "Admin"));
-            adminDAO.addAdmin(new Admin(null,"Mariet@svdj.nl","114663ab194edcb3f61d409883ce4ae6c3c2f9854194095a5385011d15becbef" //admin12
-                    ,"Admin"));
+//            adminDAO.addAdmin(new Admin(null, "Admin", "114663ab194edcb3f61d409883ce4ae6c3c2f9854194095a5385011d15becbef" //admin12
+//                    , "Admin"));
+//            adminDAO.addAdmin(new Admin(null,"Mariet@svdj.nl","114663ab194edcb3f61d409883ce4ae6c3c2f9854194095a5385011d15becbef" //admin12
+//                    ,"Admin"));
 
 //							adminDAO.addAdmin(new Admin(null,"Brandon","Noodels","Admin"));
         };
